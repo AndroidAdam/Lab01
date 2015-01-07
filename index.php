76fef88dbb363eb08812b36a0f10aa2c8e1137f7
A00860588 <A00860588@SE12L0319-06.ad.bcit.ca>
2015-01-06 17:31:43 -0800

    Updated index

--- index.php
+++ index.php
@@ -6,13 +6,46 @@
     </head>
     <body>
         <?php
-        include('student.php');
-        
+        include('Student.php');
+
         $students = array();
-        
-        foreach($students as $student)
+
+        foreach ($students as $student) {
             echo $student->toString();
+        }
+
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home', 'john@doe.com');
+        $first->add_email('work', 'jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
+
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home', 'albert@braniacs.com');
+        $second->add_email('work1', 'a_einstein@bcit.ca');
+        $second->add_email('work2', 'albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
         
+        $third = new Student();
+        $third->surname = "Jesse";
+        $third->first_name = "Adam";
+        $third->add_email('home', 'akjesse@gmail.com');
+        $third->add_email('work1', 'akjesse@gmail.com');
+        $third->add_grade(99);
+        $third->add_grade(98);
+        $third->add_grade(51);
+        $students['a588'] = $third;
+        
+
         echo 'Hello out there... I live';
         ?>
     </body>
