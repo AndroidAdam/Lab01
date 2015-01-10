6e4b20c848aed6ebbafa1f5c2204d572a998c083
Adam Jesse <ak1jesse@gmail.com>
2015-01-09 19:40:32 -0800

    Added comments/fixed style

--- Student.php
+++ Student.php
@@ -13,34 +13,38 @@
  */
 class Student {
     
-    function _construct() {
+    //Constructor for student class
+     function __construct() {
         $this->surname = '';
         $this->first_name = '';
         $this->emails = array();
         $this->grades = array();
     }
-    
+    //Adds email of a student
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
     
+    //Adds grade of a student
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    //Computes average grade of a student
     function average() {
         $total = 0;
-        foreach ($this->grades as $value)
+        foreach ($this->grades as $value) 
             $total += $value;
         return $total / count($this->grades);
     }
     
+    //String method for displaying student records
     function toString() {
-        $result = $this->first_name . '' . $this->surname;
-        $result .= '('.$this->average().")\n";
-        foreach($this->emails as $which=>$what)
-            $result .= $which .':'. $what. "\n";
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' ('.$this->average().")\n";
+        foreach($this->emails as $which => $what)
+            $result .= $which . ': '. $what . "\n";
         $result .= "\n";
         return '<pre>' . $result . '</pre>';
-    }  
+    }
 }
